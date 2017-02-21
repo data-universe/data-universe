d1b9969d3e1fb3504fe15db5bab313fc282e0011
marcus-r <marcus0706@live.se>
2017-02-21 20:56:11 +0100

    Util for loading JSON

---
+++ src/utils/loadJSON.js
@@ -0,0 +1,17 @@
+function loadJSON(path, callback) {
+  const request = new XMLHttpRequest();
+  request.overrideMimeType('application/json');
+  request.open('GET', path, true);
+  request.onload = () => {
+    if (request.status === 200) {
+      const json = JSON.parse(request.responseText);
+      callback(undefined, json);
+    } else {
+      const error = `Error ${request.status} loading ${path}`;
+      callback(error, undefined);
+    }
+  };
+  request.send(null);
+}
+
+export default loadJSON;
