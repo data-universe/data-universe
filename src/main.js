49a2947642c60397df098950e6a7fdfd14fb1ee9
marcus-r <marcus0706@live.se>
2017-02-21 01:27:01 +0100

    Setup webpack eslint and threejs

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { Scene } from 'three/src/scenes/Scene';
+import { PerspectiveCamera } from 'three/src/cameras/PerspectiveCamera';
+import { WebGLRenderer } from 'three/src/renderers/WebGLRenderer';
+import { BoxGeometry } from 'three/src/geometries/BoxGeometry';
+import { MeshBasicMaterial } from 'three/src/materials/MeshBasicMaterial';
+import { Mesh } from 'three/src/objects/Mesh';
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function render() {
+  requestAnimationFrame(render);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+render();
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.glsl$/,
+        loader: 'webpack-glsl-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      three$: path.resolve(__dirname, 'node_modules/three/src/'),
+    },
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+  })],
+};
