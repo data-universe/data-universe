80e9b32acc12524f96804fc8738dc6d05a4da951
Marcus Rettig <marcus0706@live.se>
2017-03-31 19:10:20 +0200

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
     'no-param-reassign': ['error', { 'props': false }],
     'import/prefer-default-export': 'off',
     'no-use-before-define': ['error', { 'functions': false, 'classes': true }],
+    'brace-style': 'stroustrup',
   },
 };
