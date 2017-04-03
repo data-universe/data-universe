f63ef2e303ccfdc3fffa77cacd84e5254762f663
funrep <fuuunrep@gmail.com>
2017-04-03 16:49:17 +0200

    added custom loader for CSS3DObject and basic code for billboards for planets

--- src/planet.js
+++ src/planet.js
@@ -2,6 +2,7 @@ import { Color } from 'three/math/Color';
 import { MeshLambertMaterial } from 'three/materials/MeshLambertMaterial';
 import { SphereGeometry } from 'three/geometries/SphereGeometry';
 import { Mesh } from 'three/objects/Mesh';
+import { createBillboard } from './billboard';
 
 const material = new MeshLambertMaterial({
   color: (new Color()).setHSL(0.5, 1.0, 0.5),
@@ -11,6 +12,10 @@ const geometry = new SphereGeometry(0.5, 32, 16);
 
 function createPlanet() {
   const planet = new Mesh(geometry, material);
+  const billboard = createBillboard('<h1>foobar</h1>');
+  const node = planet.position;
+  billboard.position.set(node.x, node.y + 0.5, node.z);
+  planet.add(billboard);
   return planet;
 }
 
