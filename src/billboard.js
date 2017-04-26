8beee3dc314c23853ffcc2bcdcaa85e86d2e1edf
Simon Persson <simon@akep.se>
2017-04-26 12:47:02 +0200

    Fixed Billboard design

--- src/billboard.js
+++ src/billboard.js
@@ -29,26 +29,33 @@ function createSprite(text, subtext) {
 function createTexture(text, subtext) {
   // These constants can be tweaked to change the styling.
   const fontFace = 'Arial';
-  const fontSize = 24;
+  const headerFontSize = 24;
+  const infoFontSize = 20;
   const backgroundColor = 'rgba(50,75,75,0.5)';
-  const textColor = 'rgba(255,255,255,0.75)';
+  const headerTextColor = 'rgba(255,255,255,0.75)';
+  const infoTextColor = 'rgba(255,255,255,0.60)';
   const padding = 10;
   const boxWidth = 256; // Must be a power of 2.
-  const lineHeight = 1.4 * fontSize;
   // Removes some height from the bottom of the box to make the text look more centered.
-  const heightReduction = 0.1 * fontSize;
+  const heightReduction = 0.1 * headerFontSize;
+  const headerLineHeight = 1.4 * headerFontSize;
+  const infoLineHeight = 1.4 * infoFontSize;
 
   // Setup Canvas
   const canvas = document.createElement('canvas');
   canvas.width = boxWidth;
   canvas.height = boxWidth;
   const context = canvas.getContext('2d');
-  context.font = `Normal ${fontSize}px ${fontFace}`;
 
   // Calculate properties and wrap text
   const contentWidth = boxWidth - (2 * padding);
-  const lines = wrapText(context, text, subtext, contentWidth);
-  const contentHeight = (lines.length * lineHeight) - heightReduction;
+  const headerLines = wrapText(context, text, contentWidth);
+  const infoLines = wrapText(context, subtext, contentWidth);
+
+  const spacing = 10;
+
+  const totalLineHeight = (infoLineHeight * infoLines.length) + (headerLineHeight * headerLines.length);
+  const contentHeight = (totalLineHeight + spacing) - heightReduction;
   const boxHeight = contentHeight + (2 * padding);
   const yOffset = canvas.height - boxHeight;
 
@@ -57,53 +64,54 @@ function createTexture(text, subtext) {
   context.fillRect(0, yOffset, boxWidth, boxHeight);
 
   // Render text.
-  context.fillStyle = textColor;
   context.textBaseline = 'top';
-  lines.forEach((line, i) => {
+  context.font = `Normal ${headerFontSize}px ${fontFace}`;
+  context.fillStyle = headerTextColor;
+
+  headerLines.forEach((line, i) => {
     const lineWidth = context.measureText(line).width;
     const lineX = padding + ((contentWidth - lineWidth) / 2);
-    const lineY = padding + (i * lineHeight) + yOffset;
+    const lineY = padding + (i * headerLineHeight) + yOffset;
+    context.fillText(line, lineX, lineY, contentWidth);
+  });
+
+  const headerHeight = headerLines.length * headerLineHeight;
+  const infoTextStart = spacing + headerHeight;
+
+  context.font = `Normal ${infoFontSize}px ${fontFace}`;
+  context.fillStyle = infoTextColor;
+
+  infoLines.forEach((line, i) => {
+    const lineWidth = context.measureText(line).width;
+    const lineX = padding + ((contentWidth - lineWidth) / 2);
+    const lineY = infoTextStart + padding + (i * infoLineHeight) + yOffset;
     context.fillText(line, lineX, lineY, contentWidth);
   });
 
   return new Texture(canvas);
 }
 
-function wrapText(context, text, subtext, maxWidth) {
+function wrapText(context, text, maxWidth) {
   const words = text.split(' ');
   if (words.length <= 1) {
     return [text];
   }
+
   const lines = [];
   let line = words[0];
 
-  wordsToLines(words);
-
-  function wordsToLines(w) {
-    for (let i = 1; i < w.length; i += 1) {
-      const word = w[i];
-      const testLine = `${line} ${word}`;
-      const testWidth = context.measureText(testLine).width;
-      if (testWidth >= maxWidth) {
-        lines.push(line);
-        line = word;
-      }
-      else {
-        line = testLine;
-      }
+  for (let i = 1; i < words.length; i += 1) {
+    const word = words[i];
+    const testLine = `${line} ${word}`;
+    const testWidth = context.measureText(testLine).width;
+    if (testWidth >= maxWidth) {
+      lines.push(line);
+      line = word;
     }
-    lines.push(line);
+    else {
+      line = testLine;
+    }
   }
-
-  const subwords = subtext.split(' ');
-  lines.push('------------------');
-  if (subwords.length <= 1) {
-    lines.push(subtext);
-  }
-  else {
-    line = subwords[0];
-    wordsToLines(subwords);
-  }
-
+  lines.push(line);
   return lines;
 }
