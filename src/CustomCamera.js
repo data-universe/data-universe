62d5d36eb114fd91c34b77026cd39800aac0c73b
Marcus Rettig <marcus0706@live.se>
2017-05-01 19:29:45 +0200

    Minor code fix

--- src/CustomCamera.js
+++ src/CustomCamera.js
@@ -3,6 +3,7 @@ import { PerspectiveCamera } from 'three/cameras/PerspectiveCamera';
 export default class CustomCamera extends PerspectiveCamera {
   constructor() {
     super(25, window.innerWidth / window.innerHeight, 0.1, 1e7);
+
     this.onResize = this.onResize.bind(this);
   }
 
