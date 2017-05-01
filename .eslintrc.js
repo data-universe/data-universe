b2568b39ada8497340a41a7b397004d0f0ec18f3
Marcus Rettig <marcus0706@live.se>
2017-05-01 19:15:32 +0200

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   'rules': {
     'no-param-reassign': ['error', { 'props': false }],
-    'import/prefer-default-export': 'off',
+    // 'import/prefer-default-export': 'off',
     'no-use-before-define': ['error', { 'functions': false, 'classes': true }],
     'brace-style': ['error', 'stroustrup'],
   },
