79ce7a7933d7a3f840766c14c564c0f512bcddf8
Marcus Rettig <marcus0706@live.se>
2017-05-01 22:14:24 +0200

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,6 @@ module.exports = {
     'import/resolver': 'webpack',
   },
   'rules': {
-    'no-param-reassign': ['error', { 'props': false }],
-    // 'import/prefer-default-export': 'off',
     'no-use-before-define': ['error', { 'functions': false, 'classes': true }],
     'brace-style': ['error', 'stroustrup'],
   },
