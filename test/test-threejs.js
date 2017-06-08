0fb8b87b35629567666919dcf517ba1ad08a35d4
funrep <fuuunrep@gmail.com>
2017-06-08 17:05:08 +0200

    Setup mocha-wepback runner and a basic test

--- test/test-threejs.js
+++ test/test-threejs.js
@@ -1,15 +1,22 @@
 const THREE = require('three');
 const assert = require('assert');
+const RND = require('../src/utils/random.js');
 
 
-describe('The THREE object', function() {
+describe('Basic test file', () => {
 
-  it('should have a defined BasicShadowMap constant', function() {
+
+  it('should have a defined BasicShadowMap constant', () => {
     assert.notEqual('undefined', THREE.BasicShadowMap);
-  }),
+  });
 
-  it('should be able to construct a Vector3 with default of x=0', function() {
-    var vec3 = new THREE.Vector3();
+
+  it('should be able to construct a Vector3 with default of x=0', () => {
+    const vec3 = new THREE.Vector3();
     assert.equal(0, vec3.x);
-  }),
-});
+  });
+
+  it('should not be able to be equal to 10', () => {
+    assert.notEqual(10, RND.randomInt(0, 9));
+  });
+})
