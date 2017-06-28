1b0d6b465fbf07a56c76c6404b66444dce4c9a2f
funrep <fuuunrep@gmail.com>
2017-06-28 13:13:24 +0200

    Changed lookAt logic based on quaternion, updated style of the cylinder

--- src/ui/Compass.js
+++ src/ui/Compass.js
@@ -1,29 +1,24 @@
 import { Color } from 'three/math/Color';
 import { Object3D } from 'three/core/Object3D';
-import { Vector3 } from 'three/math/Vector3';
+import { Matrix4 } from 'three/math/Matrix4';
 import { Mesh } from 'three/objects/Mesh';
-import { TetrahedronGeometry } from 'three/geometries/TetrahedronGeometry';
 import { CylinderGeometry } from 'three/geometries/CylinderGeometry';
-import { MeshLambertMaterial } from 'three/materials/MeshLambertMaterial';
+import { MeshBasicMaterial } from 'three/materials/MeshBasicMaterial';
 
 export default class Compass extends Object3D {
   constructor(spawn) {
     super();
 
-    // TODO: different colors on the faces or change geometric type
-    // const geometry = new TetrahedronGeometry(0.8);
-    const geometry = new CylinderGeometry(0, 0.2, 1, 50);
+    const geometry = new CylinderGeometry(0, 0.2, 1, 20);
     geometry.rotateZ(-Math.PI / 2);
-    //geometry.faces.map(face => face.color.setRGB(0, 0, 0));
-    //geometry.colorsNeedUpdate = true;
-    const material = new MeshLambertMaterial({
+    const material = new MeshBasicMaterial({
       color: (new Color()).setHSL(0.8, 0.4, 0.5),
       reflectivity: 1.0,
+      wireframe: true,
     });
 
     const mesh = new Mesh(geometry, material);
-    mesh.position.set(1, 1, 0);
-    mesh.position.multiplyScalar(1.2);
+    mesh.position.set(0.9, 2, 0);
     this.add(mesh);
 
     this.spawn = spawn;
@@ -32,9 +27,8 @@ export default class Compass extends Object3D {
   }
 
   update(camera) {
-    const direction = new Vector3();
-    direction.copy(this.spawn);
-    direction.sub(camera.position);
-    this.children[0].lookAt(direction);
+    const m1 = new Matrix4();
+    m1.lookAt(this.spawn, camera.position, camera.up);
+    this.children[0].quaternion.setFromRotationMatrix(m1);
   }
 }
