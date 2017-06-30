464e7362678fb981e9d65ea5234c605c3cff6974
Marcus Rettig <marcus0706@live.se>
2017-06-30 19:35:03 +0200

    Load multiple chunks in planet buffer

--- src/PlanetBuffer.js
+++ src/PlanetBuffer.js
@@ -3,6 +3,7 @@ import Chunk from './PlanetBufferChunk';
 export default class PlanetBuffer {
   constructor(scene, camera) {
     this.chunkSize = 25;
+    this.renderDistance = 2;
     this.chunks = {};
     this.lastChunkIndex = { xi: NaN, yi: NaN, zi: NaN };
     this.planets = [];
@@ -46,12 +47,41 @@ export default class PlanetBuffer {
     return chunks[xi][yi][zi];
   }
 
+  getChunksAround(centerChunkIndex) {
+    const xiStart = centerChunkIndex.xi - this.renderDistance;
+    const yiStart = centerChunkIndex.yi - this.renderDistance;
+    const ziStart = centerChunkIndex.zi - this.renderDistance;
+    const xiEnd = centerChunkIndex.xi + this.renderDistance;
+    const yiEnd = centerChunkIndex.yi + this.renderDistance;
+    const ziEnd = centerChunkIndex.zi + this.renderDistance;
+
+    const chunks = [];
+    for (let xi = xiStart; xi <= xiEnd; xi += 1) {
+      for (let yi = yiStart; yi <= yiEnd; yi += 1) {
+        for (let zi = ziStart; zi <= ziEnd; zi += 1) {
+          chunks.push(this.getChunk({ xi, yi, zi }));
+        }
+      }
+    }
+    return chunks;
+  }
+
   updatePosition(position) {
     const index = this.getChunkIndexAtPosition(position);
     const lastIndex = this.lastChunkIndex;
     if (index.xi !== lastIndex.xi || index.yi !== lastIndex.yi || index.zi !== lastIndex.zi) {
-      this.getChunk(lastIndex).unload();
-      this.getChunk(index).load();
+      const lastChunks = this.getChunksAround(lastIndex);
+      const chunks = this.getChunksAround(index);
+
+      const lastChunksSet = new Set(lastChunks);
+      const chunksSet = new Set(chunks);
+
+      const chunksToUnload = lastChunks.filter(chunk => !chunksSet.has(chunk));
+      const chunksToLoad = chunks.filter(chunk => !lastChunksSet.has(chunk));
+
+      chunksToUnload.forEach(chunk => chunk.unload());
+      chunksToLoad.forEach(chunk => chunk.load());
+
       this.lastChunkIndex = index;
     }
   }
